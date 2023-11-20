4152fb48e2592b4855d4bd694b869d79a632bba0
naoya25 <onaoya2002@gmail.com>
2023-11-20 14:48:26 +0900

    fix

---
+++ 01/src/App.js
@@ -0,0 +1,12 @@
+// App.js
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
--- 02/src/App.js
+++ 02/src/App.js
@@ -1,13 +1,19 @@
 import React, { useState } from 'react';
+import './Counter.css'; // Assuming a separate CSS file for styling
 
 function Counter() {
   const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <h1>{count}</h1>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    <div className="app-container">
+      <div className="card">
+        <h3 className="display-text">React Counter</h3>
+        <h1 className="count-display">{count}</h1>
+        <div className="button-container">
+          <button className="increment-button" onClick={() => setCount(count + 1)}>+</button>
+          <button className="decrement-button" onClick={() => setCount(count - 1)}>-</button>
+        </div>
+      </div>
     </div>
   );
 }
